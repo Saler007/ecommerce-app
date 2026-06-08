956e2811c0e4370906c4f5df98fa00ebfdd2fad5
Saler laptop <chhunpichpisal096@yahoo.com>
2026-06-08 10:10:57 +0700

    initial

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,22 @@
+import logo from "./logo.jpg";
+import heroImg from "./hero.png";
+
+export const assets = {
+  logo,
+  heroImg,
+};  
+
+export const products = [
+  {
+    _id: 1,
+    name: "Product 1",
+    description: "This is a description of Product 1.",
+    price: 19.99,
+    image: [],
+    category: "Category 1",
+    subcategory: "Subcategory 1",
+    size: ["S", "M", "L"],
+    date: 32312231,
+    bestSeller: true,
+  }
+]
